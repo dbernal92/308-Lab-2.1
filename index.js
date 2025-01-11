8de1e0c03d0c7afb8b7ddf71cda9c2efb7088a62
Darien Bernal <darien.bernal@gmail.com>
2025-01-10 23:53:28 -0500

    Fixed action threshold variables, created if/else statements for each week

--- index.js
+++ index.js
@@ -11,25 +11,54 @@ console.log(`Garden area: ${gardenArea}`)
 
 // 2. Find the maximum number of plants the garden can hold.
 let gardenCapacity = gardenArea / plantMinSpace;
-console.log(`Garden max capacity: ${Math.round(gardenCapacity)}`)
+console.log(`Garden max capacity: ${gardenCapacity} (${Math.round(gardenCapacity)})`)
 
 // 3. Predict plant growth for 1, 2, and 3 weeks.
-let plantsGrown = 0;
+let growthFirstWeek = 0;
+let growthSecondWeek = 0;
+let growthThirdWeek = 0;
 
 console.log(`Week 0: ${plantStart}`)
-    plantsGrown = plantStart * plantGrowthRate;
-console.log(`Week 1: ${plantsGrown}`)
-    plantsGrown = plantsGrown * plantGrowthRate;
-console.log(`Week 2: ${plantsGrown}`)
-    plantsGrown = plantsGrown * plantGrowthRate;
-console.log(`Week 3: ${plantsGrown}`)
+    growthFirstWeek = plantStart * plantGrowthRate;
+console.log(`Week 1: ${growthFirstWeek}`)
+    growthSecondWeek = growthFirstWeek * plantGrowthRate;
+console.log(`Week 2: ${growthSecondWeek}`)
+    growthThirdWeek = growthSecondWeek * plantGrowthRate;
+console.log(`Week 3: ${growthThirdWeek}`)
 
 // 4. Use conditions to decide whether to prune, monitor, or plant more.
-let plantedCap = gardenCapacity * .5;
+let plantMoreLimit = gardenCapacity * .5;
+let monitorLimit = gardenCapacity * .8;
 
+console.log(plantMoreLimit)
+console.log(monitorLimit)
 
-if (plantStart >= 1 && plantStart <= (gardenCapacity * .50)) {
-    console.log(planted);
-} else if (plantsGrown => ) {
+//Week 1
+console.log('Week 1:')
+if (growthFirstWeek < plantMoreLimit) {
+    console.log('Plant more');
+} else if (growthFirstWeek >= plantMoreLimit && growthFirstWeek < monitorLimit){
+    console.log('Monitor');
+} else {
+        console.log('Prune')
+}
 
+//Week 2
+console.log('Week 2:')
+if (growthSecondWeek < plantMoreLimit) {
+    console.log('Plant more');
+} else if (growthSecondWeek >= plantMoreLimit && growthSecondWeek < monitorLimit){
+    console.log('Monitor');
+} else {
+        console.log('Prune')
+}
+
+//Week 3
+console.log('Week 3:')
+if (growthThirdWeek < plantMoreLimit) {
+    console.log('Plant more');
+} else if (growthThirdWeek >= plantMoreLimit && growthThirdWeek < monitorLimit){
+    console.log('Monitor');
+} else {
+        console.log('Prune')
 }
